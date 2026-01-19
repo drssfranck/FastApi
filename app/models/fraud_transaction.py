090ecd1025a40b2830188b4573f111e2c3cf5706
Dnasty Group <153373692+DnastyGroup@users.noreply.github.com>
2026-01-19 21:52:04 +0100

    Ajout de la détection de fraude avec des routes API pour gérer les transactions frauduleuses et les statistiques associées.

---
+++ app/models/fraud_transaction.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, ConfigDict
+from typing import Optional
+from datetime import datetime
+
+class FraudTransaction(BaseModel):
+    """
+    Schéma représentant une transaction frauduleuse.
+    """
+    id: int
+    date: Optional[datetime]
+    client_id: int
+    card_id: int
+    amount: float
+    use_chip: Optional[str]
+    merchant_id: Optional[int]
+    merchant_city: Optional[str]
+    merchant_state: Optional[str]
+    zip: Optional[int]
+    mcc: Optional[int]
+    errors: Optional[str]
+    isFraud: int
+
+    # Configuration Pydantic
+    model_config = ConfigDict(from_attributes=True)
